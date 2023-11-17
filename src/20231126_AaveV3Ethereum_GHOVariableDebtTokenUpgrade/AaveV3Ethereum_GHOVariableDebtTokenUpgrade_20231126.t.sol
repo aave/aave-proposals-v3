51adc07d01b4b4d00178c6a9e973e9098e6c0695
Parth Patel <parth4321patel@gmail.com>
2023-11-17 13:33:19 +0530

    test: Add tests for update of gho variable token

--- src/20231126_AaveV3Ethereum_GHOVariableDebtTokenUpgrade/AaveV3Ethereum_GHOVariableDebtTokenUpgrade_20231126.t.sol
+++ src/20231126_AaveV3Ethereum_GHOVariableDebtTokenUpgrade/AaveV3Ethereum_GHOVariableDebtTokenUpgrade_20231126.t.sol
@@ -2,10 +2,14 @@
 pragma solidity ^0.8.0;
 
 import 'forge-std/Test.sol';
-import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV3EthereumAssets, AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
 import {ProtocolV3TestBase} from 'aave-helpers/ProtocolV3TestBase.sol';
 import {AaveV3Ethereum_GHOVariableDebtTokenUpgrade_20231126} from './AaveV3Ethereum_GHOVariableDebtTokenUpgrade_20231126.sol';
-import {GhoVariableDebtToken} from 'gho-core/contracts/facilitators/aave/tokens/GhoVariableDebtToken.sol';
+import {IGhoVariableDebtToken} from 'gho-core/contracts/facilitators/aave/tokens/interfaces/IGhoVariableDebtToken.sol';
+
+interface IGhoVariableDebtTokenHelper is IGhoVariableDebtToken {
+  function DEBT_TOKEN_REVISION() external returns (uint256);
+}
 
 /**
  * @dev Test for AaveV3Ethereum_GHOVariableDebtTokenUpgrade_20231126
@@ -29,6 +33,8 @@ contract AaveV3Ethereum_GHOVariableDebtTokenUpgrade_20231126_Test is ProtocolV3T
   }
 
   function test_debtTokenRevisionUpdate() public {
-    assertTrue(GhoVariableDebtToken(AaveV3Ethereum.GHO_V_TOKEN).DEBT_TOKEN_REVISION == 0x3);
+    assertTrue(
+      IGhoVariableDebtTokenHelper(AaveV3EthereumAssets.GHO_V_TOKEN).DEBT_TOKEN_REVISION() == 0x3
+    );
   }
 }
