93032b435873e0abec5217e405f5e0e53d0dbacc
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-08-08 19:09:38 -0300

    feat: Remote Lanes refactored using proposed abstraction

---
+++ src/20250519_Multi_GHOAvalancheLaunch/remote-lanes/Base_Avalanche_AaveV3GHOLane_20250519.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IUpgradeableBurnMintTokenPool_1_5_1} from "src/interfaces/ccip/tokenPool/IUpgradeableBurnMintTokenPool.sol";
+import {AaveV3GHOLane} from "../abstraction/AaveV3GHOLane.sol";
+import {GhoCCIPChains} from "../abstraction/constants/GhoCCIPChains.sol";
+
+/**
+ * @title GHO Avalanche Listing
+ * @author Aave Labs
+ * - Discussion: https://governance.aave.com/t/arfc-launch-gho-on-avalanche-set-aci-as-emissions-manager-for-rewards
+ * - Snapshot: https://snapshot.box/#/s:aavedao.eth/proposal/0x2aed7eb8b03cb3f961cbf790bf2e2e1e449f841a4ad8bdbcdd223bb6ac69e719
+ */
+contract Base_Avalanche_AaveV3GHOLane_20250519 is AaveV3GHOLane {
+    constructor() AaveV3GHOLane(GhoCCIPChains.BASE()) {}
+
+    function _chainLanesToAdd()
+        internal
+        pure
+        override
+        returns (IUpgradeableBurnMintTokenPool_1_5_1.ChainUpdate[] memory)
+    {
+        return _asArray(_asChainUpdateWithDefaultRateLimiterConfig(GhoCCIPChains.AVALANCHE()));
+    }
+}
