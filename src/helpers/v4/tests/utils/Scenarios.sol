12e0b1180a537bbc21300486187e0716cdc289d4
YBM <31329384+yan-man@users.noreply.github.com>
2026-03-24 16:42:38 -0500

    fix: rm logging on borrow ceiling

--- src/helpers/v4/tests/utils/Scenarios.sol
+++ src/helpers/v4/tests/utils/Scenarios.sol
@@ -214,12 +214,6 @@ abstract contract Scenarios is Helpers {
       // Use 50% of max for safety margin
       maxBorrowableAmount = maxBorrowableAmount / 2;
       borrowCeiling = testAssetAmount < maxBorrowableAmount ? testAssetAmount : maxBorrowableAmount;
-      console.log(
-        'BORROW_CEILING: maxDebt=%e, available=%e, ceiling=%e',
-        maxDebtValue,
-        maxBorrowableAmount,
-        borrowCeiling
-      );
     }
     if (borrowCeiling == 0) {
       return 0;
