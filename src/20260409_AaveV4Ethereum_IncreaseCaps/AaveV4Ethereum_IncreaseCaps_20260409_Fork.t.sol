9034c6a155a3369aa09e681e0fc175998005cf61
YBM <31329384+yan-man@users.noreply.github.com>
2026-04-09 21:53:53 -0500

    test: clean up

--- src/20260409_AaveV4Ethereum_IncreaseCaps/AaveV4Ethereum_IncreaseCaps_20260409_Fork.t.sol
+++ src/20260409_AaveV4Ethereum_IncreaseCaps/AaveV4Ethereum_IncreaseCaps_20260409_Fork.t.sol
@@ -14,11 +14,11 @@ contract AaveV4Ethereum_IncreaseCaps_20260409_ForkTest is
   AaveV4Ethereum_IncreaseCaps_20260409_Test
 {
   function setUp() public override {
-    vm.createSelectFork(vm.rpcUrl('vtestnet'), 24844481);
+    vm.createSelectFork(vm.rpcUrl('vtestnet'), 24846420);
   }
 
   // prettier-ignore
-  function test_caps_coreHub() public override {
+  function test_caps_coreHub() public view override {
     //                                                                                                                  addCap     drawCap
     _assertCaps(CORE_HUB, address(AaveV4EthereumSpokes.ETHERFI_E_SPOKE), AaveV4EthereumAssets.WETH_UNDERLYING,          0,         1_600);
     _assertCaps(CORE_HUB, address(AaveV4EthereumSpokes.ETHERFI_E_SPOKE), AaveV4EthereumAssets.weETH_UNDERLYING,         1_500,     0);
@@ -43,7 +43,7 @@ contract AaveV4Ethereum_IncreaseCaps_20260409_ForkTest is
   }
 
   // prettier-ignore
-  function test_caps_primeHub() public override {
+  function test_caps_primeHub() public view override {
     //                                                                                                                   addCap     drawCap
     _assertCaps(PRIME_HUB, address(AaveV4EthereumSpokes.BLUECHIP_SPOKE), AaveV4EthereumAssets.GHO_UNDERLYING,            2_000_000, 2_250_000);
     _assertCaps(PRIME_HUB, address(AaveV4EthereumSpokes.BLUECHIP_SPOKE), AaveV4EthereumAssets.USDC_UNDERLYING,           750_000,   875_000);
@@ -55,7 +55,7 @@ contract AaveV4Ethereum_IncreaseCaps_20260409_ForkTest is
   }
 
   // prettier-ignore
-  function test_caps_plusHub() public override {
+  function test_caps_plusHub() public view override {
     //                                                                                                                                         addCap     drawCap
     _assertCaps(PLUS_HUB, address(AaveV4EthereumSpokes.ETHENA_ECOSYSTEM_SPOKE), AaveV4EthereumAssets.GHO_UNDERLYING,                           750_000,   850_000);
     _assertCaps(PLUS_HUB, address(AaveV4EthereumSpokes.ETHENA_ECOSYSTEM_SPOKE), AaveV4EthereumAssets.PT_sUSDE_7MAY2026_UNDERLYING,             2_000_000, 0);
@@ -66,7 +66,7 @@ contract AaveV4Ethereum_IncreaseCaps_20260409_ForkTest is
   }
 
   // prettier-ignore
-  function test_creditLines() public override {
+  function test_creditLines() public view override {
     //                                                                                                                                      addCap  drawCap
     _assertCaps(CORE_HUB, address(AaveV4EthereumSpokes.ETHENA_ECOSYSTEM_SPOKE), AaveV4EthereumAssets.USDC_UNDERLYING,                       0,      250_000);
     _assertCaps(CORE_HUB, address(AaveV4EthereumSpokes.ETHENA_ECOSYSTEM_SPOKE), AaveV4EthereumAssets.USDT_UNDERLYING,                       0,      250_000);
